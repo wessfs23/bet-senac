b6869302001c6deb1125a1f32e4bf212d069e33d
wessfs23 <wesleyfsoffice@gmil.com>
2024-11-21 20:29:06 -0300

    mudando o projeto

--- banco.js
+++ banco.js
@@ -1,14 +1,9 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(
-    'apostas', 
-    'root', 
-    'senac123', 
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
-)
+const sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: './database.sqlite'
+    })
 
 async function conectarBanco() {
     try {
